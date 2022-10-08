9c33c758aa4b2ff36366e2fdc2500d3203a0127b
bookpark <bkbkgg@gmail.com>
2022-10-09 02:59:38 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+// showCart
+document.querySelector('.toggle-cart').addEventListener(
+    'click', () => {
+        document.querySelector('.cart-overlay').classList.add('show');
+    }
+);
+
+// closeCart
+document.querySelector('.cart-close').addEventListener(
+    'click', () => {
+        document.querySelector('.cart-overlay').classList.remove('show');
+    }
+)
+
+// showSide
+document.querySelector('.toggle-nav').addEventListener(
+    'click', () => {
+        document.querySelector('.sidebar-overlay').classList.add('show');
+    }
+)
+
+//closeSideBarClickedProducts
+document.querySelector('.product-link').addEventListener(
+    'click', () => {
+        document.querySelector('.sidebar-overlay').classList.remove('show');
+    }
+)
