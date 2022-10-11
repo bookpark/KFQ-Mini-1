4ce6f32d9c4e993a5980cfc4be3e4b97c2c1026e
bookpark <bkbkgg@gmail.com>
2022-10-11 13:29:08 +0900

    제품 이미지 클릭 시 모달창 생성 기능 추가

--- app.js
+++ app.js
@@ -57,3 +57,33 @@ document.querySelector('.cart-close').addEventListener(
         document.querySelector('body').classList.remove('overflow-hidden');
     }
 )
+
+// getProductImagesSrc
+images = document.querySelectorAll('.product-container img');
+let imgSrc;
+images.forEach((img) => {
+    img.addEventListener('click', (e) => {
+        imgSrc = e.target.src;
+        imgModal(imgSrc);
+    });
+})
+
+// createImageModal
+let imgModal = (src) => {
+    // 이미지 모달창 생성
+    const modal = document.createElement('div');
+    modal.setAttribute('class', 'modal');
+
+    // 모달 닫기 창 생성
+    const modalClose = document.createElement('i');
+    modalClose.setAttribute('class', 'bi bi-x-circle modalClose')
+    modalClose.onclick = () => {
+        modal.remove();
+    };
+
+    // shop 섹션에 모달 추가
+    document.querySelector('.shop').append(modal);
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', src);
+    modal.append(newImage, modalClose);
+}
